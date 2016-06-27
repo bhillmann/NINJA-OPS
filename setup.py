4cad9134b1dc5d5fa0f4fea11d01e749fd4c8f6c
Benjamin Hillmann <bhillmann@outlook.com>
2016-06-27 11:54:05 -0500

    Fixed the entry point.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'ninja_ops' = 'ninja_ops.scripts:main',
+            'ninja_ops = ninja_ops.scripts.ninja_ops:main',
         ]
     },
 )
